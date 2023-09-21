8868d582e7e6c238a01d76aba009045c0686678b
tomas <tomas@terc.cz>
2023-09-21 09:42:41 +0200

    21.9. Oprava chyb kvůli nasazení

--- components/AuthContextProvider.tsx
+++ components/AuthContextProvider.tsx
@@ -4,19 +4,16 @@ import React, { FC, PropsWithChildren } from 'react';
 import { firebase_app } from '../firebase/config';
 
 const auth = getAuth(firebase_app);
-type AuthContextType = { user?: User; loading?: boolean };
-export const AuthContext = React.createContext<AuthContextType>({});
+
+type AuthContextType = { user: User | null; loading?: boolean };
+export const AuthContext = React.createContext<AuthContextType>({ user: null });
 export const useAuthContext = () => React.useContext(AuthContext);
 export const AuthContextProvider: FC<PropsWithChildren> = ({ children }) => {
   const [user, setUser] = React.useState<User | null>(null);
   const [loading, setLoading] = React.useState(true);
   React.useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setUser(user);
-      } else {
-        setUser(null);
-      }
+      setUser(user); // Firebase can return null, so it's safe to set it directly
       setLoading(false);
     });
     return () => unsubscribe();
