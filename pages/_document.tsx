ac8b695c7e181980516cacefe8629f79df45d325
tomas <tomas@terc.cz>
2024-03-24 21:26:25 +0100

    24.3. Změna titlu stránky

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,6 +16,7 @@ export default class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
+          <title>Discit</title>
           <link rel="icon" href="/media/web-icon.svg" type="image/svg"/>
         </Head>
         <body>
